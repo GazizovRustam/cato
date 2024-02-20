281772c8a7472b0896d77deba750e9f69866bcb7
Газизов Рустам Рафаилович <gazizov@ONIL-ATO.local>
2024-02-20 14:57:04 +0300

    test 1

--- client/dist/scripts/index.js
+++ client/dist/scripts/index.js
@@ -2,16 +2,33 @@
 function changeStateBurgerButton(){
   
   const burgerButton = document.getElementById('burger_button');
-  const headerBurger = document.querySelectorAll('.header_burger, .header_menu');  
+  const headerBurger = document.querySelectorAll('.header_burger, .header_menu');
+  const headerBody = document.querySelector('.header_body');
+  const headerList = document.querySelectorAll('.header_list li');
   const body = document.querySelector('body');
 
   document.addEventListener('DOMContentLoaded', () =>{
+  
     burgerButton.addEventListener('click', (e) => {
         headerBurger.forEach((element) => {
           element.classList.toggle('active')
         });
+        
       body.classList.toggle('lock');
+      headerBody.classList.toggle('active')
     });
+
+
+    headerList.forEach((e) => {
+      e.addEventListener('click', () => {
+        headerBurger.forEach((el) => {
+          el.classList.remove('active');
+          body.classList.remove('lock');
+          headerBody.classList.remove('active');
+        })
+      });
+    });
+
   });
 };
 changeStateBurgerButton();
